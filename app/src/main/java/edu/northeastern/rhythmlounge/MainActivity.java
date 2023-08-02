95851aa0319fa0647aad52e90341cef2996bbd1c
Ruiqi Liu <liu.ruiqi3@northeastern.edu>
2023-08-02 14:01:19 -0400

    Added Google sign in

--- app/src/main/java/edu/northeastern/rhythmlounge/MainActivity.java
+++ app/src/main/java/edu/northeastern/rhythmlounge/MainActivity.java
@@ -1,25 +1,41 @@
 package edu.northeastern.rhythmlounge;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTextEmail, editTextPassword;
+    private static final int RC_SIGN_IN = 9001; // Request code for Google sign in
+    private GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +43,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("538248299353-25nkq6svfmd0qeepdar8udgs1kap8mrt.apps.googleusercontent.com") //Web client ID
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Add click listener to Google sign-in button
+        findViewById(R.id.google_sign_in_button).setOnClickListener(view -> signInWithGoogle());
+
+
 
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
@@ -80,6 +109,92 @@ public class MainActivity extends AppCompatActivity {
         if (currentUser != null) {
             startActivity(new Intent(MainActivity.this, SelfUserPageActivity.class));
             finish();
+        }
+    }
+
+
+    private void signInWithGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account); // passing the entire GoogleSignInAccount
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                Toast.makeText(MainActivity.this, "Google sign in failed.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(MainActivity.this, SelfUserPageActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                // This exception is thrown if a user tries to sign in with a Google account that has the same email
+                                // as a user that was already created with another sign-in method.
+
+                                // Ask the user if they want to link the accounts
+                                new AlertDialog.Builder(MainActivity.this)
+                                        .setTitle("Account Linking")
+                                        .setMessage("An account with this email address already exists. Do you want to link these accounts?")
+                                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                linkAccounts(credential);
+                                            }
+                                        })
+                                        .setNegativeButton("No", null)
+                                        .show();
+                            } else {
+                                Log.w(TAG, "signInWithCredential:failure", task.getException());
+                                Toast.makeText(MainActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void linkAccounts(AuthCredential newCredential) {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            currentUser.linkWithCredential(newCredential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "linkWithCredential:success");
+                                FirebaseUser user = task.getResult().getUser();
+                                startActivity(new Intent(MainActivity.this, SelfUserPageActivity.class));
+                            } else {
+                                Log.w(TAG, "linkWithCredential:failure", task.getException());
+                                Toast.makeText(MainActivity.this, "Account linking failed.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
         }
     }
 }
