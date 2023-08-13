6904574d32850005218d5a23790e62eb9845bf1e
Ruiqi Liu <liu.ruiqi3@northeastern.edu>
2023-08-13 19:51:33 -0400

    Improve efficiency and fluency for post

--- app/src/main/java/edu/northeastern/rhythmlounge/Posts/CommentAdapter.java
+++ app/src/main/java/edu/northeastern/rhythmlounge/Posts/CommentAdapter.java
@@ -12,8 +12,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -22,11 +22,12 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Transaction;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import edu.northeastern.rhythmlounge.OtherUserPageActivity;
@@ -36,8 +37,9 @@ import edu.northeastern.rhythmlounge.UserProfileActivity;
 
 public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentViewHolder> {
 
-    private List<Comment> comments;
+    private final List<Comment> comments;
     private final Context context;
+    private final Map<String, User> userCache = new HashMap<>();
 
     public CommentAdapter(List<Comment> comments, Context context) {
         this.comments = comments;
@@ -48,7 +50,7 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
     @Override
     public CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.comment_item, parent, false);
-        return new CommentViewHolder(view);
+        return new CommentViewHolder(view, this);
     }
 
     @Override
@@ -63,42 +65,46 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
         // By default, hide the delete button
         holder.deleteButton.setVisibility(View.GONE);
 
-        // Fetch the user's details from Firestore.
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("users").document(comment.getUserId())
-                .get()
-                .addOnSuccessListener(documentSnapshot -> {
-                    if (documentSnapshot.exists()) {
-                        User user = documentSnapshot.toObject(User.class);
-                        assert user != null;
-                        holder.usernameTextView.setText(user.getUsername());
+        User cachedUser = userCache.get(comment.getUserId());
+        if (cachedUser != null) {
+            holder.usernameTextView.setText(cachedUser.getUsername());
+            Glide.with(holder.itemView.getContext())
+                    .load(cachedUser.getProfilePictureUrl())
+                    .placeholder(R.drawable.avatar)
+                    .into(holder.profileImageView);
+        } else {
+            // Fetch the user's details from Firestore.
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            db.collection("users").document(comment.getUserId())
+                    .get()
+                    .addOnSuccessListener(documentSnapshot -> {
+                        if (documentSnapshot.exists()) {
+                            User user = documentSnapshot.toObject(User.class);
+                            assert user != null;
+                            holder.usernameTextView.setText(user.getUsername());
 
-                        if (comment.getUserId().equals(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())) {
-                            holder.deleteButton.setVisibility(View.VISIBLE);
-                            holder.deleteButton.setOnClickListener(v -> {
+                            if (comment.getUserId().equals(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())) {
+                                holder.deleteButton.setVisibility(View.VISIBLE);
+                                holder.deleteButton.setOnClickListener(v -> deleteComment(comment.getPostId(), comment.getCommentId()));
+                            }
 
-                                deleteComment(comment.getPostId(), comment.getCommentId());
-                            });
+                            // Load the profile image.
+                            Glide.with(holder.itemView.getContext())
+                                    .load(user.getProfilePictureUrl())
+                                    .placeholder(R.drawable.avatar)
+                                    .into(holder.profileImageView);
                         }
+                        User user = documentSnapshot.toObject(User.class);
+                        userCache.put(comment.getUserId(), user);
 
-                        // Load the profile image.
-                        Glide.with(holder.itemView.getContext())
-                                .load(user.getProfilePictureUrl())
-                                .placeholder(R.drawable.avatar)
-                                .into(holder.profileImageView);
-                    }
+                        holder.profileImageView.setOnClickListener(v -> goToUserProfile(comment.getUserId()));
 
-                    holder.profileImageView.setOnClickListener(v -> {
-                        goToUserProfile(comment.getUserId());
-                    });
+                        holder.usernameTextView.setOnClickListener(v -> goToUserProfile(comment.getUserId()));
 
-                    holder.usernameTextView.setOnClickListener(v -> {
-                        goToUserProfile(comment.getUserId());
-                    });
-
-                    Log.d("CommentAdapter", "CommentID: " + comment.getCommentId() + ", PostID: " + comment.getPostId());
-                })
-                .addOnFailureListener(e -> Log.e("CommentAdapter", "Error fetching user details: " + e.getMessage()));
+                        Log.d("CommentAdapter", "CommentID: " + comment.getCommentId() + ", PostID: " + comment.getPostId());
+                    })
+                    .addOnFailureListener(e -> Log.e("CommentAdapter", "Error fetching user details: " + e.getMessage()));
+            }
     }
 
     @Override
@@ -113,19 +119,79 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
         ImageView profileImageView;
         ImageButton deleteButton;
 
-        public CommentViewHolder(@NonNull View itemView) {
+        public CommentViewHolder(@NonNull View itemView, final CommentAdapter adapter) {
             super(itemView);
             usernameTextView = itemView.findViewById(R.id.comment_username);
             commentTextView = itemView.findViewById(R.id.comment_content);
             profileImageView = itemView.findViewById(R.id.comment_profile_image);
             timeAgoTextView = itemView.findViewById(R.id.comment_time_ago);
             deleteButton = itemView.findViewById(R.id.delete_button);
+
+            deleteButton.setOnClickListener(v -> {
+                int position = getBindingAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    Comment currentComment = adapter.comments.get(position);
+                    adapter.deleteComment(currentComment.getPostId(), currentComment.getCommentId());
+                }
+            });
+
+            profileImageView.setOnClickListener(v -> {
+                int position = getBindingAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    Comment currentComment = adapter.comments.get(position);
+                    adapter.goToUserProfile(currentComment.getUserId());
+                }
+            });
+
+            usernameTextView.setOnClickListener(v -> {
+                int position = getBindingAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    Comment currentComment = adapter.comments.get(position);
+                    adapter.goToUserProfile(currentComment.getUserId());
+                }
+            });
+        }
+    }
+
+    private static class CommentDiffCallback extends DiffUtil.Callback {
+        private final List<Comment> oldComments;
+        private final List<Comment> newComments;
+
+        public CommentDiffCallback(List<Comment> oldComments, List<Comment> newComments) {
+            this.oldComments = oldComments;
+            this.newComments = newComments;
+        }
+
+        @Override
+        public int getOldListSize() {
+            return oldComments.size();
+        }
+
+        @Override
+        public int getNewListSize() {
+            return newComments.size();
+        }
+
+        @Override
+        public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+            return oldComments.get(oldItemPosition).getCommentId().equals(newComments.get(newItemPosition).getCommentId());
+        }
+
+        @Override
+        public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+            return oldComments.get(oldItemPosition).equals(newComments.get(newItemPosition));
         }
     }
 
     public void setComments(List<Comment> newComments) {
-        this.comments = newComments;
+        CommentDiffCallback diffCallback = new CommentDiffCallback(this.comments, newComments);
+        DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(diffCallback);
+
+        this.comments.clear();
+        this.comments.addAll(newComments);
+        diffResult.dispatchUpdatesTo(this);
     }
+
 
     private String getTimeAgo(Date date) {
         if (date == null) return "";
@@ -136,8 +202,8 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
         if (timeDifference < 60 * 1000) {
             return "just now";
         } else if (timeDifference < 60 * 60 * 1000) {
-            int mins = (int) (timeDifference / (60 * 1000));
-            return mins + "m ago";
+            int minutes = (int) (timeDifference / (60 * 1000));
+            return minutes + "m ago";
         } else if (timeDifference < 24 * 60 * 60 * 1000) {
             int hours = (int) (timeDifference / (60 * 60 * 1000));
             return hours + "h ago";
@@ -178,9 +244,7 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
                                 Toast.makeText(context, "Comment deleted", Toast.LENGTH_SHORT).show();
                                 decrementPostCommentCount(postId);
                             })
-                            .addOnFailureListener(e -> {
-                                Toast.makeText(context, "Error deleting comment: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                            });
+                            .addOnFailureListener(e -> Toast.makeText(context, "Error deleting comment: " + e.getMessage(), Toast.LENGTH_SHORT).show());
                 })
                 .setNegativeButton("No", null)  // No action for "No"
                 .show();
@@ -190,20 +254,17 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference postRef = db.collection("posts").document(postId);
 
-        db.runTransaction(new Transaction.Function<Void>() {
-                    @Nullable
-                    @Override
-                    public Void apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
-                        DocumentSnapshot snapshot = transaction.get(postRef);
+        db.runTransaction((Transaction.Function<Void>) transaction -> {
+            DocumentSnapshot snapshot = transaction.get(postRef);
 
-                        // Get the current comment count
-                        long currentCount = snapshot.getLong("commentCount");
-                        if (currentCount > 0) {
-                            transaction.update(postRef, "commentCount", currentCount - 1);
-                        }
-                        return null;
-                    }
-                })
+            // Get the current comment count
+                    Long count = snapshot.getLong("commentCount");
+                    long currentCount = (count != null) ? count : 0;
+                    if (currentCount > 0) {
+                transaction.update(postRef, "commentCount", currentCount - 1);
+            }
+            return null;
+        })
                 .addOnSuccessListener(aVoid -> Log.d("CommentAdapter", "Comment count decremented!"))
                 .addOnFailureListener(e -> Log.e("CommentAdapter", "Error decrementing comment count", e));
     }
